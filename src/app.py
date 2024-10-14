95a72800c9a5f880661d17e9aedc0394ae9bf390
ivan dochev <ivan.v.dochev@gmail.com>
2024-10-14 14:27:47 +0800

    Uptaded the main files

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.get('/')
+@app.route('/')
+@app.route('/home')
 def hello_world():
-    return "hello,world"
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=1)
