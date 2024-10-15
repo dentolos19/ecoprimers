662f965abf5bd731bb57d6b8da066f5cc3a21f3f
ivan dochev <ivan.v.dochev@gmail.com>
2024-10-15 12:02:51 +0800

    added comments explaining app.route

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# @app.route is basically telling the website what function it should run when you switch to a website. 
+# in this case, going to the example of placeholder.com/ or placeholder.com/home will both run the home_page() function)
 @app.route("/")
 @app.route("/home")
 def home_page():
