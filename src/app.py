7e111b394ccab07f203abd344be0a4a6106f7de8
ivan dochev <ivan.v.dochev@gmail.com>
2024-10-15 16:46:25 +0800

    reorganised the code so split everything up so that the app.py file only contains the run script and everything is seperated out

--- src/app.py
+++ src/app.py
@@ -1,15 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# @app.route is basically telling the website what function it should run when you switch to a website. 
-# in this case, going to the example of placeholder.com/ or placeholder.com/home will both run the home_page() function)
-@app.route("/")
-@app.route("/home")
-def home_page():
-    return render_template("home.html")
-
+from App import app
 
 if __name__ == "__main__":
     # Debug mode will be turned on via the launch configuration
-    app.run()
+    app.run(debug=1)
