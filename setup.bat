5d30ee32814847acb690e783d7b5868d3e4144f9
Dennise Catolos <dentolos19@gmail.com>
2024-10-16 18:32:28 +0800

    Updated configurations

--- setup.bat
+++ setup.bat
@@ -1,13 +1,11 @@
 @echo off
-
 cd /d %~dp0
-cd ..
 
 echo Setting up virtual environment...
-
 if not exist ".venv" (
     py -m venv ".venv"
 )
-
 call .venv\Scripts\activate.bat
+
+echo Checking requirements...
 pip install -r "requirements.txt" >nul
