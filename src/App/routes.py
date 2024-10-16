75c244be8d8f3a13263c8d6eaa25ba0db4685895
Dennise Catolos <dentolos19@gmail.com>
2024-10-16 18:31:51 +0800

    Formatted internal code

--- src/App/routes.py
+++ src/App/routes.py
@@ -1,9 +1,11 @@
-from App import app
 from flask import render_template
 
-# @app.route is basically telling the website what function it should run when you switch to a website. 
+from App import app
+
+
+# @app.route is basically telling the website what function it should run when you switch to a website.
 # in this case, going to the example of placeholder.com/ or placeholder.com/home will both run the home_page() function)
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home_page():
     return render_template("home.html")
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from App import app
 
 if __name__ == "__main__":
-    # Debug mode will be turned on via the launch configuration
-    app.run(debug=1)
+    # Debug mode will be set via the environment variable (FLASK_DEBUG)
+    app.run()
