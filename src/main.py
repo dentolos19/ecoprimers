3ece194fca0e48604e277e01483da6f68249f91d
Ivan dochev <ivan.v.dochev@gmail.com>
2024-11-09 12:53:31 +0800

    Trying to set up a database but its not working for some reason

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///src/users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secret-key"
+db = SQLAlchemy(app)
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 if __name__ == "__main__":
     app.run()
