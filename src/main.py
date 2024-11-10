5b0b17418f0fcfd20004f387df5a47468a6b81fc
Dennise Catolos <dentolos19@gmail.com>
2024-11-10 10:21:17 +0800

    for ivan

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 #the app folder was removed and the code was restructured to help with sqlite implementation
 
+import os
+from os import path
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///src/users.db"
+
+DATABASE_PATH = path.join(app.root_path, "instance", "users.db")
+# ensure that the directory for the database file exists
+if not path.exists(path.dirname(DATABASE_PATH)):
+    os.makedirs(path.dirname(DATABASE_PATH))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DATABASE_PATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret-key"
 db = SQLAlchemy(app)
