d5b903078c436cfe705beee41f26f67ab5c61fca
ivan <ivan.v.dochev@gmail.com>
2025-01-11 12:12:30 +0800

    transferring current work to my laptop

--- src/static/js/messages.js
+++ src/static/js/messages.js
@@ -1,11 +1,18 @@
-function open_chat() {
-    console.log("function accessed")
+function open_chat(event) {
+    const clickedUser = event.target.closest('.user');
+    
+    if (clickedUser) {
+        const allUserDivs = document.querySelectorAll('.user');
+        allUserDivs.forEach(div => div.classList.remove('active'));
+        
+        clickedUser.classList.add('active');
+    }
 }
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const usersDiv = document.querySelector("#users");
     if (usersDiv) {
-        usersDiv.addEventListener("click", open_chat);
+        usersDiv.addEventListener("click", open_chat);  
     }
 });
+
