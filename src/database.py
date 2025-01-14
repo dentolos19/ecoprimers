d96e3b5846ec0e3154672a47954b5b71ef7f73f9
Dennise Catolos <dentolos19@gmail.com>
2025-01-14 19:40:21 +0800

    Added database setup

--- src/database.py
+++ src/database.py
@@ -23,6 +23,8 @@ def init(app: Flask, local: bool = True):
     if initialized:
         return
 
+    first_setup = False
+
     # Load environment variables
     app.config["TURSO_DATABASE_URL"] = os.environ.get("TURSO_DATABASE_URL")
     app.config["TURSO_AUTH_TOKEN"] = os.environ.get("TURSO_AUTH_TOKEN")
@@ -35,6 +37,11 @@ def init(app: Flask, local: bool = True):
         # Ensure that the directory for the database exists
         if not os.path.exists(database_dir):
             os.makedirs(database_dir)
+
+        # Check if the database file exists; if not, pre-create data
+        if not os.path.exists(database_file):
+            first_setup = True
+
         url = "sqlite:///" + database_file
     else:
         url = f"sqlite+{app.config['TURSO_DATABASE_URL']}/?authToken={app.config['TURSO_AUTH_TOKEN']}&secure=true"
@@ -60,3 +67,22 @@ def init(app: Flask, local: bool = True):
         migrate = Migrate(app, sql)
 
         initialized = True
+
+        if first_setup:
+            setup(app, sql)
+
+
+def setup(app: Flask, sql: SQLAlchemy):
+    from models import Event, User
+
+    # Create the data
+    user_admin = User(username="admin", email="admin@appdev.proj", password="admin")
+    dennise_user = User(username="dennise", email="dennise@duck.com", password="Dennise!123")
+    event = Event(title="Event 1", description="Description 1", location="Location 1", date="2025-01-01")
+
+    # Commit the changes
+    with app.app_context():
+        sql.session.add(user_admin)
+        sql.session.add(dennise_user)
+        sql.session.add(event)
+        sql.session.commit()
