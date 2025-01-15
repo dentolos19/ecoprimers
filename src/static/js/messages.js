6d44164fce0f3e49ca98a50ebcc24426857418f3
ivan <ivan.v.dochev@gmail.com>
2025-01-15 11:31:59 +0800

    commiting to move to laptop

--- src/static/js/messages.js
+++ src/static/js/messages.js
@@ -6,48 +6,51 @@ let currentRecipientId = null;
 let currentSenderId = null;
 
 function displayMessage(messageData) {
-  const messageSpace = document.querySelector(".message-space");
-  const messageBlock = document.createElement("div");
-  messageBlock.className = `message-block ${messageData.sender_id === currentSenderId ? "you" : "other-person"}`;
+	const messageSpace = document.querySelector(".message-space");
+	const messageBlock = document.createElement("div");
+	messageBlock.className = `message-block ${messageData.sender_id === currentSenderId ? "you" : "other-person"}`;
 
-  messageBlock.innerHTML = `
-    <div class="message">
-      <p>${messageData.message}</p>
-      <span class="timestamp">${new Date(messageData.sent_time).toLocaleTimeString()}</span>
-    </div>
+	messageBlock.innerHTML = `
+	<form class = "edit-button" action = {{ url_for("edit_message(id = ${messageData.id}") }} method = "get">
+		<div class="message">
+			<p>${messageData.message}</p>
+			<span class="timestamp">Read status: ${messageData.is_read ? "Read" : "Unread"}</span>
+		</div>
+		<button value = "Edit message" type = "submit">
+	</form>
   `;
 
-  messageSpace.appendChild(messageBlock);
-  messageSpace.scrollTop = messageSpace.scrollHeight;
+	messageSpace.appendChild(messageBlock);
+	messageSpace.scrollTop = messageSpace.scrollHeight;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  currentSenderId = document.querySelector("#receiver-id").value;
-  currentRecipientId = document.querySelector("#sender-id").value;
+	currentSenderId = document.querySelector("#receiver-id").value;
+	currentRecipientId = document.querySelector("#sender-id").value;
 
-  fetch(`/api/messages?sender_id=${currentSenderId}&receiver_id=${currentRecipientId}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((messages) => {
-      messages.forEach((message) => displayMessage(message));
-    });
+	fetch(`/api/messages?sender_id=${currentSenderId}&receiver_id=${currentRecipientId}`)
+		.then((response) => {
+			return response.json();
+		})
+		.then((messages) => {
+			messages.forEach((message) => displayMessage(message));
+		});
 
-  // Socket event listeners
-  socket.on("connect", () => {
-    console.log("Connected to Socket.IO server");
-  });
+	// Socket event listeners
+	socket.on("connect", () => {
+		console.log("Connected to Socket.IO server");
+	});
 
-  socket.on("receive_message", (data) => {
-    // TODO: Fix socket updates
-    const messageSpace = document.querySelector(".message-space");
-    messageSpace.innerHTML = "";
-    fetch(`/api/messages?sender_id=${currentSenderId}&receiver_id=${currentRecipientId}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((messages) => {
-        messages.forEach((message) => displayMessage(message));
-      });
-  });
+	socket.on("receive_message", (data) => {
+		// TODO: Fix socket updates
+		const messageSpace = document.querySelector(".message-space");
+		messageSpace.innerHTML = "";
+		fetch(`/api/messages?sender_id=${currentSenderId}&receiver_id=${currentRecipientId}`)
+			.then((response) => {
+				return response.json();
+			})
+			.then((messages) => {
+				messages.forEach((message) => displayMessage(message));
+			});
+	});
 });
