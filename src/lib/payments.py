e51eaec8f9b0af2cbfc5629a3720ee5bc4515e6e
Dennise Catolos <dentolos19@gmail.com>
2025-02-03 18:46:48 +0800

    Updated payments

--- src/lib/payments.py
+++ src/lib/payments.py
@@ -18,3 +18,24 @@ def init(app: Flask):
     stripe.api_key = app.config["STRIPE_SECRET_KEY"]
 
     initialized = True
+
+
+def pay(amount: float, success_url: str, cancel_url: str):
+    return stripe.checkout.Session.create(
+        payment_method_types=["card", "paynow"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "sgd",
+                    "product_data": {
+                        "name": "Donation",
+                    },
+                    "unit_amount": int(amount * 100),
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
