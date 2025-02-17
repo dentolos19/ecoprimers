5e868eb62d5a36921823d900db9c8f8bd3d4e977
Dennise Catolos <dentolos19@gmail.com>
2025-02-17 14:47:09 +0800

    Added profile followings and followers list

--- src/routing/profile.py
+++ src/routing/profile.py
@@ -10,6 +10,7 @@ from utils import require_login
 @require_login
 def profile():
     user_id = session.get("user_id")
+
     user = sql.session.query(User).filter(User.id == user_id).first()
     events = sql.session.query(Event).join(EventAttendee).filter(EventAttendee.user_id == user_id).all()
     posts = sql.session.query(Post).filter(Post.user_id == user_id).all()
@@ -29,14 +30,37 @@ def profile():
     )
 
 
-@app.route("/profile/<id>")
+@app.route("/profile/<id>", methods=["GET", "POST"])
+@require_login
 def profile_other(id):
+    user_id = session.get("user_id")
+
+    if request.method == "POST":
+        following = (
+            sql.session.query(UserFollow).filter(UserFollow.user_id == id, UserFollow.follower_id == user_id).first()
+        )
+
+        if following:
+            sql.session.delete(following)
+            flash("You have unfollowed this user.", "success")
+        else:
+            sql.session.add(UserFollow(user_id=id, follower_id=user_id))
+            flash("You have followed this user.", "success")
+
+        sql.session.commit()
+
+        return redirect(request.referrer)
+
     user = sql.session.query(User).filter(User.id == id).first()
     events = sql.session.query(Event).join(EventAttendee).filter(EventAttendee.user_id == id).all()
     posts = sql.session.query(Post).filter(Post.user_id == id).all()
     followers = sql.session.query(UserFollow).filter(UserFollow.user_id == id).count()
     followings = sql.session.query(UserFollow).filter(UserFollow.follower_id == id).count()
     transactions = sql.session.query(Transaction).filter(Transaction.user_id == id).count()
+
+    following = (
+        sql.session.query(UserFollow).filter(UserFollow.user_id == id, UserFollow.follower_id == user_id).first()
+    )
 
     return render_template(
         "profile.html",
@@ -46,6 +70,8 @@ def profile_other(id):
         followers=followers,
         followings=followings,
         transactions=transactions,
+        editable=user_id == id,
+        following=following is not None,
     )
 
 
@@ -72,3 +98,19 @@ def profile_edit():
             sql.session.rollback()
 
     return render_template("profile-edit.html", user=user)
+
+
+@app.route("/profile/<id>/followers")
+@require_login
+def profile_followers(id):
+    followers = sql.session.query(UserFollow).filter(UserFollow.user_id == id).all()
+
+    return render_template("profile-followers.html", followers=followers)
+
+
+@app.route("/profile/<id>/followings")
+@require_login
+def profile_followings(id):
+    followings = sql.session.query(UserFollow).filter(UserFollow.follower_id == id).all()
+
+    return render_template("profile-followings.html", followings=followings)
